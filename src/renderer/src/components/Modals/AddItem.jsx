92656134d3ad182c4c2e7b291a85e5c527af4f5b
User <vazro053@gmail.com>
2025-03-11 21:35:19 +0100

    finished add item modal

--- src/renderer/src/components/Modals/AddItem.jsx
+++ src/renderer/src/components/Modals/AddItem.jsx
@@ -4,9 +4,43 @@ import Modal from './Modal';
 
 export default function AddItem() {
   const { isOpen, setIsOpen } = useModal();
+  const handleInput = (e) => {
+    const value = e.target.value;
+    if(value < 1){
+      e.target.value = 1;
+    }
+  };
+
   return(
     <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
-      This is modal AddItem
+      <form  className='p-2' >
+        <h1 className='font-medium text-xl'>Add New Item</h1>
+        <p className='text-gray-400 font-normal mt-1'>Add a new item to monitor here</p>
+        <div className='mt-5 space-y-3'>
+          {/* Name and Description*/}
+          <div>
+            <label htmlFor='name' className='font-medium'>Name</label>
+            <input type='text' id='name' className='w-full border border-gray-200 rounded-md p-2' />
+          </div>
+          <div>
+            <label htmlFor='description' className='font-medium'>Description</label>
+            <input type='text' id='description' className='w-full border border-gray-200 rounded-md p-2' />
+          </div>
+          {/* Numbers Block */}
+          <div className='flex flex-row'>
+            <div className='w-1/2'>
+              <label htmlFor='quantity' className='font-medium'>Quantity</label>
+              <input type='number' id='quantity' className='w-full border border-gray-200 rounded-md p-2' defaultValue={1} onInput={handleInput}/>
+            </div>
+            <div className='w-1/2'>
+              <label htmlFor='price' className='font-medium'>Price</label>
+              <input type='number' id='price' className='w-full border border-gray-200 rounded-md p-2' defaultValue={1} onInput={handleInput}/>
+            </div>
+          </div>
+          {/* Button */}
+          <button className='bg-black hover:bg-gray-800 py-2 px-4 text-white subpixel-antialiased rounded-md block ml-auto'>Add Item</button>
+        </div>
+      </form>
     </Modal>
   )
 }
--- src/renderer/src/components/Modals/Modal.jsx
+++ src/renderer/src/components/Modals/Modal.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import { X } from 'lucide-react';
 
 export default function Modal({ isOpen, onClose, children }) {
   if (!isOpen) return null;
+
   return (
-    <div className='fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center'>
-      <div className='bg-white p-4 rounded-md shadow-md'>
-        <button onClick={onClose} className='absolute top-2 right-2'>
-          ✖
+    <div className='fixed inset-0 flex justify-center items-center'>
+      <div className='absolute inset-0 bg-black opacity-80' onClick={onClose}></div>
+
+      <div className='bg-white p-4 rounded-md shadow-md w-md relative'>
+        <button onClick={onClose} className='absolute top-6 right-6'>
+        <X className='text-gray-800 h-5 w-5 font-thin' />
         </button>
         {children}
       </div>
     </div>
-  )
+  );
 }
